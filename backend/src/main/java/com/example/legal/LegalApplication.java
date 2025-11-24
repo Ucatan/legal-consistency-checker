3ed5a620d85da66f4ec8025d19776e9522b665f0
Adrew Gusev <telemailbox@gmail.com>
2025-11-24 15:28:20 +0500

    LegalApplicaiton class added

---
+++ backend/src/main/java/com/example/legal/LegalApplication.java
@@ -0,0 +1,14 @@
+package com.example.legal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LegalApplication.class, args);
+    }
+
+}
